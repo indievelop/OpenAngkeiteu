f61a6aa57375ae572102e0204aea52d1d6c84b11
Kim Yeon Bong <yearsalary@gmail.com>
2017-11-26 01:37:37 +0900

    Angkeiteu 에서 .col .s12 .l3 제거.

--- src/components/Angkeiteu.js
+++ src/components/Angkeiteu.js
@@ -11,31 +11,29 @@ class Angkeiteu extends React.Component {
     const {data} = this.props;
 
     return (
-      <div className='col s12 l3'>
-        <div className='card small'>
-          <div className='card-content'>
-            <div className='row'>
-              <div className='col 12'>
-                <div className='card-title'>{data.title}</div>
-              </div>
+      <div className='card small'>
+        <div className='card-content'>
+          <div className='row'>
+            <div className='col 12'>
+              <div className='card-title'>{data.title}</div>
             </div>
           </div>
-          <div className='card-content'>
-            <div className='row'>
-              <div className='col s12'>
-                {data.viewCount} views
-              </div>
-              <div className='col s12'>
-                <TimeAgo date={data.createdDate}/>
-              </div>
-              <div className='col s12'>
-                {data.description}
-              </div>
+        </div>
+        <div className='card-content'>
+          <div className='row'>
+            <div className='col s12'>
+              {data.viewCount} views
+            </div>
+            <div className='col s12'>
+              <TimeAgo date={data.createdDate}/>
+            </div>
+            <div className='col s12'>
+              {data.description}
             </div>
           </div>
-          <div className='card-action'>
-            <Link to={`/readAngkeiteu/${data._id}`}>open</Link>
-          </div>
+        </div>
+        <div className='card-action'>
+          <Link to={`/readAngkeiteu/${data._id}`}>open</Link>
         </div>
       </div>
     );
