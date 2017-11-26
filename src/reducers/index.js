8cec39c466fb243e08375eb5307c65802377758f
Kim Yeon Bong <yearsalary@gmail.com>
2017-11-26 16:20:33 +0900

    targAngkeiteu action 제거.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,8 @@
 import authentication from './authentication';
 import angkeiteu from './angkeiteu';
 import search from './search';
-import targetAngkeiteu from './targetAngkeiteu';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
-  authentication, angkeiteu, search, targetAngkeiteu
+  authentication, angkeiteu, search
 });
