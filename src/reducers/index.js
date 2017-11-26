f218113a603d064922dd0c8ec8ebd2a5c341b14a
Kim Yeon Bong <yearsalary@gmail.com>
2017-11-26 22:45:43 +0900

    코멘트 리듀서 인데스 추가

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import authentication from './authentication';
 import angkeiteu from './angkeiteu';
+import comment from './comment';
 import search from './search';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
-  authentication, angkeiteu, search
+  authentication, angkeiteu, comment, search
 });
