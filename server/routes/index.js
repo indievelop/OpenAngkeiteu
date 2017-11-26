6d2c88681ca37234231b6c93984ecef683b7d295
Kim Yeon Bong <yearsalary@gmail.com>
2017-11-26 22:45:59 +0900

    코멘트 라우트 인덱스 추가.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import account from './account';
 import angkeiteu from './angkeiteu';
+import comment from './comment';
 
 const router = express.Router();
 router.use('/account', account);
 router.use('/angkeiteu', angkeiteu);
+router.use('/comment', comment);
 
 export default router;
