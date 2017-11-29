c5a9e0114a24e8bd53f5a3e6733103f97f8f604c
Kim Yeon Bong <yearsalary@gmail.com>
2017-11-29 19:33:20 +0900

    코멘트 추천(recommendation) 속성 추가.

--- server/models/comment.js
+++ server/models/comment.js
@@ -6,6 +6,10 @@ const Comment = new Schema({
   angkeiteuId: {type: Schema.Types.ObjectId, ref: 'angkeiteu'},
   writer: String,
   content: String,
+  recommendations:[{
+    accountId: {type: Schema.Types.ObjectId, ref: 'account'},
+    recommendationDate: {type: Date , default: Date.now}
+  }],
   createdDate: {type: Date, default: Date.now}
 });
 
