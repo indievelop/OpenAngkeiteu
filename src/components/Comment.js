f0de0b9b05f66744c7e73e7a375c4d270006da0a
Kim Yeon Bong <yearsalary@gmail.com>
2017-11-29 19:46:15 +0900

    코멘트 추천 버튼 추가 및 요청 관련 핸들러 추가, 요청 성공(SUCCESS)후 데이터 변경 적용에 관련 코드 추가.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import TimeAgo from 'react-timeago';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { recommendCommentRequest } from 'actions/comment';
+
 
 class Comment extends React.Component {
   constructor(props) {
       super(props);
+      this.state = {
+        data: props.data
+      };
+      this.handleRecommendComment = this.handleRecommendComment.bind(this);
+  }
+
+  componentDidMount() {
+    let nextState = {};
+    nextState['data'] = this.props.data;
+    this.setState(nextState);
+  }
+
+  handleRecommendComment() {
+    this.props.recommendCommentRequest(this.props.data._id).then(() => {
+      if(this.props.recommendCommentStatus.status === 'SUCCESS') {
+        this.setState({data: this.props.recommendCommentStatus.data});
+      } else {
+        let errorMessage = [
+          'You are not logged in.',
+          'Wrong Comment id.',
+          'Duplicated Recommend.',
+        ];
+        let $toastContent = $('<span style="color: #FFB4BA">' + errorMessage[this.props.recommendCommentStatus.error-1] + '</span>');
+        Materialize.toast($toastContent, 2000);
+      }
+    });
   }
 
   render() {
-    const {data} = this.props;
+    const {data} = this.state;
 
     return (
       <div className='row'>
@@ -19,9 +48,33 @@ class Comment extends React.Component {
         <div className='col s12'>
           {data.content}
         </div>
+        <div className='col s10'>
+          recommendations: {data.recommendations.length}
+        </div>
+        <div className='col s2'>
+          <a className='waves-effect waves-light btn-floating'
+             onClick={this.handleRecommendComment}>
+            <i className='material-icons md-dark'>
+              thumb_up
+            </i>
+          </a>
+        </div>
       </div>
     );
   }
 }
 
-export default Comment;
+const mapStateToProps = (state) => {
+  return {
+    recommendCommentStatus: state.comment.recommend
+  };
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    recommendCommentRequest: (id) => {
+      return dispatch(recommendCommentRequest(id));
+    }
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Comment);
