b7adc51effc2d6edad8392863538e4e8a2832f5e
Kim Yeon Bong <yearsalary@gmail.com>
2017-12-15 01:36:08 +0900

    파일 업로드시 파일 모델 생성 로직 추가, 파일 모델(path 를 포함한) 에 대한 Get 요청 api 추가.

--- server/routes/file.js
+++ server/routes/file.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import File from '../models/file';
 import mongoose from 'mongoose';
 import multer from 'multer';
 
@@ -8,22 +9,55 @@ const storage = multer.diskStorage({
     cb(null, 'uploads')
   },
   filename: function (req, file, cb) {
-    cb(null, `${req.body.id}.${file.mimetype.split('/')[1]}`);
+    file.uploadedFile = {
+      name: req.body.connectedObj_id,
+      ext: file.mimetype.split('/')[1]
+    };
+    file.uploadedFile.path = `/uploads/${file.uploadedFile.name}.${file.uploadedFile.ext}`;
+    cb(null, `${file.uploadedFile.name}.${file.uploadedFile.ext}`);
   }
 });
 const fileFilter = (req, file, cb) => {
-  if(!mongoose.Types.ObjectId.isValid(req.body.id))
+  if(!mongoose.Types.ObjectId.isValid(req.body.connectedObj_id))
     return cb(new Error('INVAILD ID'));
   cb(null, true);
 }
 const upload = multer({ 'storage': storage, 'fileFilter': fileFilter}).single('imgFile');
 
 // UPLOAD IMAGE_FILE
-router.post('/image', (req, res)=>{
+router.post('/image', (req, res) =>{
+  // CHECK LOGIN STATUS
+  if(typeof req.session.loginInfo === 'undefined') {
+      return res.status(403).json({
+          errorMessage: "NOT LOGGED IN"
+      });
+  }
+  // UPLOAD
   upload(req, res, (err) => {
     if(err) return res.status(400).json({'errorMessage': err.message});
-    return res.json({success: true});
+
+    let file = new  File({
+      connectedObj: {
+        kind: req.body.connectedObj_kind,
+        id: req.body.connectedObj_id
+      },
+      path: req.file.uploadedFile.path
+    });
+    console.log(file);
+    file.save((err, newFile) => {
+      if(err) throw err;
+      return res.json({ success: true, id: newFile._id});
+    });
   });
+});
+
+// GET File
+router.get('/image', (req, res) => {
+  File.findOne(req.query)
+  .exec((err, file) => {
+    if(err) throw err;
+    return res.json(file);
+  })
 });
 
 export default router;
