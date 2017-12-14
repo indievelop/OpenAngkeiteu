6b15e58a80efe61f3f7ecd58d6e41d6784151d63
Kim Yeon Bong <yearsalary@gmail.com>
2017-12-15 01:39:11 +0900

    이미지 뷰가 objId 를 받으면 imageFileGetRequest 를 통해 업로드된 이미지 파일 path를 포함한 정보를 얻게 수정.

--- src/components/ImageView.js
+++ src/components/ImageView.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import CommentForm from 'components';
+import { imageFileGetRequest } from 'actions/file';
 
 class ImageView extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploadedImagePath: ''
+    }
+  }
 
   componentDidMount() {
     $(document).ready(function(){
@@ -10,9 +19,20 @@ class ImageView extends React.Component {
     });
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(typeof nextProps.objId !== 'undefined' && nextProps.objId !== this.props.objId)
+      this.props.imageFileGetRequest(nextProps.objId);
+
+    if(nextProps.imageFileGetStatus !== this.props.imageFileGetStatus) {
+      let nextState = {};
+      nextState['uploadedImagePath'] = nextProps.imageFileGetStatus.data.path;
+      this.setState(nextState);
+    }
+  }
+
   render() {
     let {src, height, width, onRemoveClick} = this.props;
-
+    let { uploadedImagePath } = this.state;
     let defaults = {
       height: height || 100,
       width: width || 100,
@@ -20,13 +40,25 @@ class ImageView extends React.Component {
 
     return (
       <div>
-        <img className='materialboxed responsive-img' src={src} {...defaults}/>
+        <img className='materialboxed responsive-img' src={uploadedImagePath !== '' ? uploadedImagePath : src} {...defaults}/>
         {typeof onRemoveClick !== 'undefined' ? <a className='btn' onClick={onRemoveClick}>remove</a> :undefined}
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+    return {
+      imageFileGetStatus: state.file.get
+    };
+};
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        imageFileGetRequest: (objId) => {
+            return dispatch(imageFileGetRequest(objId));
+        }
+    };
+};
 
-export default ImageView;
+export default connect(mapStateToProps, mapDispatchToProps)(ImageView);
