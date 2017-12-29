bca3679b464530d4d0cbe285679abccce62b4b1e
Kim Yeon Bong <yearsalary@gmail.com>
2017-12-29 15:15:02 +0900

    이미지뷰어 액션 타입, 액션 크리에이터, 리듀서 추가.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,9 +5,10 @@ import search from './search';
 import file from './file';
 import chartFilter from './chartFilter';
 import angkeiteuExplorer from './angkeiteuExplorer';
+import imageViewer from './imageViewer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
   authentication, angkeiteu, comment, search, file, chartFilter,
-  angkeiteuExplorer
+  angkeiteuExplorer, imageViewer
 });
