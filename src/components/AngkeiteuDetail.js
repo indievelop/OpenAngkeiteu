e7dc57afeb762d41d411d72e18be75252cd4a0f0
Kim Yeon Bong <yearsalary@gmail.com>
2017-12-30 23:04:02 +0900

    앙케이트 디테일을 child (List, Option, SelectBtn) 에 의해서 구성 되도록 수정.

--- src/components/AngkeiteuDetail.js
+++ src/components/AngkeiteuDetail.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import TimeAgo from 'react-timeago';
-import { AngkeiteuHeader, OptionList, ImageView } from 'components';
+import { AngkeiteuHeader, ImageView } from 'components';
 
 class AngkeiteuDetail extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.renderChildren = this.renderChildren.bind(this);
+  }
+
+  renderChildren() {
+    let optionsElements = null;
+    let selectBtnElement = null;
+    React.Children.map(this.props.children, child => {
+      if(child.type.name === 'List') {
+        optionsElements = React.cloneElement(child, {
+          'data': this.props.data.options
+        });
+      } else if(child.type.name === 'SelectBtn') {
+        selectBtnElement = React.cloneElement(child);
+      }
+    });
+    return (
+      <div>
+        <div className='card-content'>
+          options
+        </div>
+        <div className='card-content'>
+          {optionsElements}
+        </div>
+        <div className='card-action'>
+          {selectBtnElement}
+        </div>
+      </div>
+    );
+  }
+
   render() {
     let {data} = this.props;
 
@@ -26,11 +59,7 @@ class AngkeiteuDetail extends React.Component {
         <div className='card-content'>
           {typeof data._id !== 'undefined' ? <ImageView objId={data._id} width={400} height={400}/> : undefined}
         </div>
-        <div className='card-content'>
-          options
-        </div>
-        <div className='card-content'>
-        </div>
+        {this.renderChildren()}
       </div>
     );
   }
