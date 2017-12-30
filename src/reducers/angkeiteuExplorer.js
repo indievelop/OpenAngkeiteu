a21223f8a454973b2a1c413c9be6c592cd5ecd28
Kim Yeon Bong <yearsalary@gmail.com>
2017-12-30 23:12:04 +0900

    init, option select, complete 리듀서 처리 추가.

--- src/reducers/angkeiteuExplorer.js
+++ src/reducers/angkeiteuExplorer.js
@@ -2,7 +2,9 @@ import * as types from 'actions/ActionTypes';
 import update from 'react-addons-update';
 
 const initialState = {
-  selectedAngkeiteu: {}
+  status: 'INIT',
+  selectedAngkeiteu: {},
+  selectedOption: {}
 }
 
 export default function angkeiteuExplorer(state, action) {
@@ -11,14 +13,31 @@ export default function angkeiteuExplorer(state, action) {
   }
 
   switch (action.type) {
+    case types.ANGKEITEU_EXPLORER_INIT:
+      return update(state, {
+        status: { $set: 'INIT' },
+        selectedAngkeiteu: { $set: {} },
+        selectedOption: { $set: {} }
+      });
     case types.ANGKEITEU_EXPLORER_ANGKEITEU_SELECT:
       return update(state, {
-        selectedAngkeiteu: { $set: action.data}
+        status: { $set: 'WAITING' },
+        selectedAngkeiteu: { $set: action.data }
       });
     case types.ANGKEITEU_EXPLORER_ANGKEITEU_UNSELECT:
       return update(state, {
+        status: { $set: 'WAITING' },
         selectedAngkeiteu: { $set: {} }
-      })
+      });
+    case types.ANGKEITEU_EXPLORER_OPTION_SELECT:
+      return update(state, {
+        status: { $set: 'WAITING' },
+        selectedOption: { $set: action.data }
+      });
+    case types.ANGKEITEU_EXPLORER_COMPLETE:
+      return update(state, {
+        status: { $set: 'COMPLETE' }
+      });
     default:
       return state;
   }
