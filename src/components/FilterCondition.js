ee72a863501d33da0b63743dabe0a31e68797a54
Kim Yeon Bong <yearsalary@gmail.com>
2018-01-02 18:21:10 +0900

    children SelectBtn 을 위한 renderChildren() 추가.

--- src/components/FilterCondition.js
+++ src/components/FilterCondition.js
@@ -2,15 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class FilterCondition extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderChildren = this.renderChildren.bind(this);
+  }
+
+  renderChildren() {
+    return React.Children.map(this.props.children, child => {
+      if(child.type.name ==='SelectBtn')
+        return React.cloneElement(child, {
+          'data': this.props.data
+        });
+      else
+        return undefined;
+    });
+  }
+  
   render() {
     let {data} = this.props;
     return (
       <div className='row'>
-        <div className='col s6'>
+        <div className='col s5'>
           {data.angkeiteu.title}
         </div>
-        <div className='col s6'>
+        <div className='col s5'>
           {data.option.description}
+        </div>
+        <div className='col s2'>
+          {this.renderChildren()}
         </div>
       </div>
     );
