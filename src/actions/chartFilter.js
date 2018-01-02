dc2bddfd6f50c23db2add38789762f8804dd699e
Kim Yeon Bong <yearsalary@gmail.com>
2018-01-02 15:48:16 +0900

    차트 필터 피터링 액션 크리에이터 추가.

--- src/actions/chartFilter.js
+++ src/actions/chartFilter.js
@@ -1,6 +1,7 @@
 import {
   CHART_FILTER_CONDITION_ADD,
-  CHART_FILTER_CONDITION_REMOVE
+  CHART_FILTER_CONDITION_REMOVE,
+  CHART_FILTER_FILTERING
 } from './ActionTypes';
 
 export function addChartFilterCondition(filterCondition) {
@@ -14,5 +15,12 @@ export function removeChartFilterCondition(filterCondition) {
   return {
     type: CHART_FILTER_CONDITION_REMOVE,
     data: filterCondition
+  }
+}
+
+export function filtering(originParticipants) {
+  return {
+    type: CHART_FILTER_FILTERING,
+    data: originParticipants
   }
 }
