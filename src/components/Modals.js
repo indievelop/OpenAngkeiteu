94951c015bcbb29eee2d2f42a4b06005575be618
Kim Yeon Bong <yearsalary@gmail.com>
2018-01-19 18:05:00 +0900

    AngkeiteuCreatorModal 을 AngkeiteuFormModal 로 변경.

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import { ImageViewerModal, AngkeiteuExplorerModal, AngkeiteuCreatorModal } from 'components';
+import React from 'react'
+import { ImageViewerModal, AngkeiteuExplorerModal } from 'components'
+import { AngkeiteuFormModal } from 'containers'
 
 class Modals extends React.Component {
-
   componentDidMount() {
     //modal Initialization.
     $(document).ready(() => {
       $('.modal').modal({
         ready: (modal, trigger) => { modal.scrollTop(0); }
-      });
-    });
+      })
+    })
   }
 
   render() {
-    let {history} =this.props;
-    
+    const {history} =this.props
     return (
       <div>
-        <AngkeiteuCreatorModal history={history}/>
+        <AngkeiteuFormModal {...{history}} />
         <AngkeiteuExplorerModal/>
         <ImageViewerModal/>
       </div>
-    );
+    )
   }
 }
 
-export default Modals;
+export default Modals
