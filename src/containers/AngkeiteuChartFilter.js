cbf0545ac62083d5c5b2288a31be5a1c7c0dc674
Kim Yeon Bong <yearsalary@gmail.com>
2018-01-21 23:21:11 +0900

    AngkeiteuChartFilter 컴포넌트, 컨테이너 코드 분리.

---
+++ src/containers/AngkeiteuChartFilter.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { AngkeiteuChartFilter} from 'components'
+import { addChartFilterCondition, filtering, init } from 'actions/chartFilter'
+import { init as initExplorer } from 'actions/angkeiteuExplorer'
+
+class AngkeiteuChartFilterContainer extends React.Component {
+  componentDidMount() {
+    this.props.init()
+    this.handleOnAngkeiteuExplorer = this.handleOnAngkeiteuExplorer.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.angkeiteuExplorerStatus !== this.props.angkeiteuExplorerStatus) {
+      if(nextProps.angkeiteuExplorerStatus.status === 'COMPLETE') {
+        let {selectedAngkeiteu, selectedOption} = nextProps.angkeiteuExplorerStatus
+        this.props.addChartFilterCondition(selectedAngkeiteu, selectedOption)
+      }
+    }
+
+    if(nextProps.chartFilterStatus.conditions !== this.props.chartFilterStatus.conditions) {
+      this.props.filtering(this.props.data.participants)
+    }
+  }
+
+  handleOnAngkeiteuExplorer() {
+    //find modal on
+    this.props.initExplorer('Add FilterCondtion')
+    $('#angkeiteuExplorerModal').modal('open')
+  }
+
+  render() {
+    const {conditions} = this.props.chartFilterStatus
+    const {handleOnAngkeiteuExplorer} = this
+    return (
+      <AngkeiteuChartFilter {...{conditions, handleOnAngkeiteuExplorer}}/>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+      chartFilterStatus: state.chartFilter,
+      angkeiteuExplorerStatus: state.angkeiteuExplorer,
+      data: state.angkeiteu.get.data
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addChartFilterCondition: (angkeiteu, option) => {
+      return dispatch(addChartFilterCondition(angkeiteu, option))
+    },
+    filtering: (originParticipants) => {
+      return dispatch(filtering(originParticipants))
+    },
+    init: () => {
+      return dispatch(init())
+    },
+    initExplorer: (purpose) => {
+      return dispatch(initExplorer(purpose))
+    }
+  }
+}
+
+export default connect (mapStateToProps, mapDispatchToProps)(AngkeiteuChartFilterContainer)
