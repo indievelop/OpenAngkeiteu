5c249a291844cd5a465b6ac5d8c106287f0390ee
Kim Yeon Bong <yearsalary@gmail.com>
2018-01-22 20:05:41 +0900

    CommentForm 컴포넌트, 컨테이너 코드 분리.

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,50 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { commentPostRequest, commentListRequest } from 'actions/comment';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { commentPostRequest, commentListRequest } from 'actions/comment'
 
 class CommentForm extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: ''
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handlePost = this.handlePost.bind(this);
-    this.initFormData = this.initFormData.bind(this);
-  }
-
-  handleChange(e) {
-    let nextState = {};
-    nextState[e.target.name] = e.target.value;
-    this.setState(nextState);
-  }
-
-  initFormData() {
-    let nextState = {};
-    nextState['content'] = '';
-    this.setState(nextState);
-  }
-
-  handlePost() {
-    this.props.commentPostRequest(this.props.angkeiteuId, this.state.content).then(() => {
-      if(this.props.commentPostStatus.status === 'SUCCESS') {
-        this.initFormData();
-        this.props.commentListRequest(true, this.props.angkeiteuId);
-      } else {
-        let errorMessage = [
-          'You are not logged in.',
-          'Wrong Angkeiteu id.',
-          'Please write content.',
-        ];
-        let $toastContent = $('<span style="color: #FFB4BA">' + errorMessage[this.props.commentPostStatus.error-1] + '</span>');
-        Materialize.toast($toastContent, 2000);
-      }
-    });
-  }
-
   render() {
+    const {content, handleChange, handlePost} = this.props
     return (
       <div className='row'>
         <div className='input-field col s12'>
@@ -53,37 +13,25 @@ class CommentForm extends React.Component {
                     name='content'
                     type='text'
                     className='materialize-textarea validate'
-                    onChange={this.handleChange}
-                    value={this.state.content}>
+                    onChange={handleChange}
+                    value={content}>
           </textarea>
         </div>
         <div className='col s2 offset-s10'>
           <a className="waves-effect waves-light btn"
-             onClick={this.handlePost}>
+             onClick={handlePost}>
              <i className="material-icons center">comment</i>
           </a>
         </div>
       </div>
-    );
+    )
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    authenticateStatus: state.authentication.status,
-    commentPostStatus: state.comment.post
-  };
+CommentForm.propTypes = {
+  content: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handlePost: PropTypes.func.isRequired
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    commentPostRequest: (angkeiteuId, content) => {
-      return dispatch(commentPostRequest(angkeiteuId, content));
-    },
-    commentListRequest: (isInitial, angkeiteuId, listType, id, email) => {
-      return dispatch(commentListRequest(isInitial, angkeiteuId, listType, id, email));
-    }
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CommentForm);
+export default CommentForm
